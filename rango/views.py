cc55b1939feb5d254708cdb911f7661e5e9c0b16
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2024-01-31 23:05:07 +0000

    end of chapter 4

--- rango/views.py
+++ rango/views.py
@@ -7,5 +7,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    html ="Go to the " + '<a href="/rango/">Index</a>' + " view"
-    return HttpResponse(html)
+    context_dict = {'yourname': 'Matthew Ballantyne',}
+    return render(request, 'rango/about.html', context=context_dict)
