77010d83162ec85effeea192bdb3feee9004b1d4
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2024-01-31 21:05:15 +0000

    URL Responses

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    html ="Go to the " + '<a href="/rango/about/">About</a>' + "view"
+    return HttpResponse(html)
+
+def about(request):
+    html ="Go to the " + '<a href="/rango/">Index</a>' + "view"
+    return HttpResponse(html)
